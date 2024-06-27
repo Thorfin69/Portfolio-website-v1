5ea728e4a08c197c3003b19c17c26d25ee08c7cf
Thorfin <136186831+Thorfin69@users.noreply.github.com>
2024-06-27 07:59:34 +0530

    Update Stakes.jsx

--- src/components/Stakes.jsx
+++ src/components/Stakes.jsx
@@ -4,7 +4,7 @@ export default function Stakes() {
     return (
         <>
             <div>
-                <h1 className='ms:mt-4 brrr text-2xl justify-center flex align-middle md:text-4xl md:justify-center ' >What i use ?</h1>
+                <h1 className=' mt-7 ms:mt-7 brrr text-2xl justify-center flex align-middle md:text-4xl md:justify-center ' >What i use ?</h1>
             </div>
             <div className='sm:grid sm:justify-center mb-4 sm:mt-4  md:flex brrr justify-center gap-3 align-middle mt-5 flex-initial' >
                 <p>HTML</p>
